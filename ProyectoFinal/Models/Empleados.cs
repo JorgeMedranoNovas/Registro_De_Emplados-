9c4c3ec78e8cd69f60b5ab865d58e64424793117
Deivi13 <Deivii.g4life@gmail.com>
2022-12-10 13:16:11 -0400

    Template code added

---
+++ ProyectoFinal/Models/Empleados.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoFinal.Models
+{
+    public class Empleados
+    {
+        [Key]
+        public int idEmpleado { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public int telefono { get; set; }
+        public string direccion { get; set; }
+        public int fechaNacimiento { get; set; }
+        public int sueldo { get; set; }
+    }
+}
---
+++ ProyectoFinal/Models/EmpleadosDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoFinal.Models
+{
+    public class EmpleadosDbContext : DbContext
+    {
+        public DbSet<Empleados> Empleados { get; set; }
+    }
+}
